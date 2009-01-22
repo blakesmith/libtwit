f58a0a701ddf933c332b9c3a08ec253a4b2e902c
Blake Smith <blakesmith0@gmail.com>
2009-01-21 23:02:48 -0500

    Broken storage of parse_user_timeline into tweet struct.

--- libtwit.c
+++ libtwit.c
@@ -13,28 +13,62 @@ xmlDocPtr open_user_timeline()
 	return doc;
 }
 
+struct tweet *create_tweet(struct tweet *previous_node)
+{
+	struct tweet *newTweet;
+	newTweet = malloc(sizeof(struct tweet));
+
+	newTweet->prev = previous_node;
+	return newTweet;
+}
+
+
 void parse_user_timeline(xmlDocPtr doc, xmlNodePtr cur)
 {
-	xmlChar *id;
+	xmlChar *value;
 	xmlNodePtr children;
+	struct tweet *current_tweet = NULL;
+	struct tweet *previous_tweet = NULL;
 
 	for (cur = cur->xmlChildrenNode; cur != NULL; cur = cur->next)
 	{
 		if ((!xmlStrcmp(cur->name, (const xmlChar *)"status")))
 		{
+			previous_tweet = current_tweet; /* First loop this is NULL */
+			current_tweet = create_tweet(previous_tweet);
+
 			for (children = cur->children; children != NULL; children = children->next)
 			{
-				if ((!xmlStrcmp(children->name, (const xmlChar *)"user")))
-					printf("User operations go here\n");
-				else {
-					id = xmlNodeGetContent(children);
-					printf("%s: %s\n", children->name, id);
-					xmlFree(id);
-				}
+				value = xmlNodeGetContent(children);
+			//	if ((!xmlStrcmp(children->name, (const xmlChar *)"user")))
+//					printf("User operations go here\n");
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"created_at")))
+					current_tweet->created_at = (char *) value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"id")))
+					current_tweet->id = (const int)value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"text")))
+					current_tweet->text = value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"source")))
+					current_tweet->source = value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"truncated")))
+					current_tweet->truncated = (const int)value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"in_reply_to_status_id")))
+					current_tweet->in_reply_to_status_id = (const int)value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"in_reply_to_user_id")))
+					current_tweet->in_reply_to_user_id = (const int)value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"favorited")))
+					current_tweet->favorited = (const int)value;
+				if ((!xmlStrcmp(children->name, (const xmlChar *)"in_reply_to_screen_name")))
+					current_tweet->in_reply_to_screen_name = value;
+//					printf("%s: %s\n", children->name, value);
+				xmlFree(value);
 			}
+			printf("%s\n", value);
+			printf("%s\n", current_tweet->created_at);
 		}
 	}
 }
+
 
 /*CURL *(setup_curl_handle)
 {
--- libtwit.h
+++ libtwit.h
@@ -1,17 +1,3 @@
-struct tweet
-{
-	char *created_at;
-	int id;
-	char *text;
-	char *source;
-	int truncated;
-	int in_reply_to_status_id;
-	int in_reply_to_user_id;
-	int favorited;
-	char *in_reply_to_screen_name;
-	struct user;
-};
-
 struct twitter_user
 {
 	int id;
@@ -24,3 +10,20 @@ struct twitter_user
 	int prot;
 	int follower_count;
 };
+
+struct tweet
+{
+	struct tweet *prev;
+	struct tweet *next;
+	char *created_at;
+	int id;
+	char *text;
+	char *source;
+	int truncated;
+	int in_reply_to_status_id;
+	int in_reply_to_user_id;
+	int favorited;
+	char *in_reply_to_screen_name;
+	struct twitter_user user;
+};
+
