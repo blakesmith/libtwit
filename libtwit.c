47c41e4b7787d95ee20e4fea707b0f965a0d6bb7
Blake Smith <blakesmith0@gmail.com>
2009-01-22 03:02:25 -0500

    destroy_tweets() added to cleanup all created tweets. display_tweets() added to walk the linked list and show the information.

--- libtwit.c
+++ libtwit.c
@@ -22,6 +22,14 @@ struct tweet *create_tweet(struct tweet *previous_node)
 	return newTweet;
 }
 
+void destroy_tweets(struct tweet *starting_tweet)
+{
+	struct tweet *i;
+
+	for (i = starting_tweet; i->next != NULL; i++)
+		free(i);
+}
+
 char *get_node_value(xmlNodePtr parent, char *search_string)
 {
 	char *value;
@@ -48,9 +56,10 @@ int sanitize_string_bool(xmlChar *test_string)
 		return -1;
 }
 
-void parse_user_timeline(xmlDocPtr doc, xmlNodePtr cur)
+struct tweet *parse_user_timeline(xmlDocPtr doc, xmlNodePtr cur)
 {
 	xmlNodePtr children;
+	struct tweet *starting_tweet;
 	struct tweet *current_tweet = NULL;
 	struct tweet *previous_tweet = NULL;
 
@@ -61,7 +70,13 @@ void parse_user_timeline(xmlDocPtr doc, xmlNodePtr cur)
 			previous_tweet = current_tweet; /* First loop this is NULL */
 			current_tweet = create_tweet(previous_tweet);
 
+			if (previous_tweet != NULL)
+				previous_tweet->next = current_tweet;
+			else
+				starting_tweet = current_tweet;
+
 //			current_tweet->user = populate_user_data()
+			current_tweet->prev = previous_tweet;
 			current_tweet->created_at = get_node_value(cur, "created_at");
 			current_tweet->id = atoi(get_node_value(cur, "id"));
 			current_tweet->text = get_node_value(cur, "text");
@@ -71,13 +86,37 @@ void parse_user_timeline(xmlDocPtr doc, xmlNodePtr cur)
 			current_tweet->in_reply_to_user_id = atoi(get_node_value(cur, "in_reply_to_user_id"));
 			current_tweet->favorited = sanitize_string_bool(get_node_value(cur, "favorited"));
 			current_tweet->in_reply_to_screen_name = get_node_value(cur, "in_reply_to_screen_name");
-			printf("========================\n");
-			printf("%i\n", current_tweet->id);
-			printf("%i\n", current_tweet->truncated);
-			printf("%i\n", current_tweet->favorited);
-			printf("%s\n", current_tweet->created_at);
-			printf("%s\n", current_tweet->text);
 		}
+	}
+	return starting_tweet;
+}
+
+void display_tweets(struct tweet *starting_tweet)
+{
+	struct tweet *i;
+
+	for (i = starting_tweet; i->next != NULL; i = i->next)
+	{
+		printf("created_at: %s\n"
+			"id: %i\n"
+			"text: %s\n"
+			"source: %s\n"
+			"truncated: %i\n"
+			"in_reply_to_status_id: %i\n"
+			"in_reply_to_user_id: %i\n"
+			"favorited: %i\n"
+			"in_reply_to_screen_name: %s\n", 
+			
+			i->created_at,
+			i->id,
+			i->text,
+			i->source,
+			i->truncated,
+			i->in_reply_to_status_id,
+			i->in_reply_to_user_id,
+			i->favorited,
+			i->in_reply_to_screen_name);
+		printf("===================================\n");
 	}
 }
 
@@ -134,10 +173,13 @@ int main(int argc, char *argv[])
 {
 	xmlDocPtr doc;
 	xmlNodePtr cur;
+	struct tweet *starting_tweet;
 
 	doc = open_user_timeline();
 	cur = xmlDocGetRootElement(doc);
-	parse_user_timeline(doc, cur);
+	starting_tweet = parse_user_timeline(doc, cur);
+	display_tweets(starting_tweet);
+	destroy_tweets(starting_tweet);
 	xmlFreeDoc(doc);
 
 	return 0;
